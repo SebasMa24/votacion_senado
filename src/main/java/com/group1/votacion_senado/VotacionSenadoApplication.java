d2c1757eca20f1543df743e2774feb94fb224a91
Sebastian Martinez <Luismartinez24210@gmail.com>
2025-10-01 23:08:35 -0500

    Initial commit

---
+++ src/main/java/com/group1/votacion_senado/VotacionSenadoApplication.java
@@ -0,0 +1,13 @@
+package com.group1.votacion_senado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotacionSenadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotacionSenadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group1/votacion_senado/VotacionSenadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.votacion_senado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotacionSenadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
