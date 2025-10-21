f0e8b2ce798a00598fad3d5342d1e83d78979372
Sebastian Martinez <Luismartinez24210@gmail.com>
2025-10-20 19:25:52 -0500

    Fix in VotanteService
    Ahora cuando se vota, se refresca la autenticación para que guarde el nuevo estado de ha_votado

--- src/main/java/com/group1/votacion_senado/service/VotanteService.java
+++ src/main/java/com/group1/votacion_senado/service/VotanteService.java
@@ -1,6 +1,9 @@
 package com.group1.votacion_senado.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.stereotype.Service;
 
@@ -26,6 +29,20 @@ public class VotanteService implements UserDetailsService {
         Votante votante = findByUsername(username);
         votante.setHaVotado(true);
         votanteRepository.save(votante);
+
+        refrescarAuthentication(votante);
+    }
+
+    public void refrescarAuthentication(Votante votante) {
+       Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+       if(auth != null && auth.isAuthenticated() && auth.getName().equals(votante.getUsername())) {
+           Authentication newAuth = new UsernamePasswordAuthenticationToken(
+               votante,
+               auth.getCredentials(),
+               votante.getAuthorities()
+           );
+           SecurityContextHolder.getContext().setAuthentication(newAuth);
+       }
     }
 }
 
